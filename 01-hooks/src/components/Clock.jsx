d92f6d51636194a06fbe4fce1d188f99dd790f44
Johan Nordström <johan@digitalvillage.se>
2021-08-24 15:24:28 +0200

    Clocks'r'us

--- 01-hooks/src/components/Clock.jsx
+++ 01-hooks/src/components/Clock.jsx
@@ -4,10 +4,18 @@ const Clock = () => {
 	const [time, setTime] = useState("13:37:00")
 
 	useEffect(() => {
-		console.log("🕰🔨 Clock is mounted 😊")
+		console.log("🕰🔨 Clock is mounted 😊 Timer started ⏲")
+
+		const intervalId = setInterval(() => {
+			// Update time with a tick
+			console.log('tick')
+			const now = new Date();
+			setTime(now.toLocaleTimeString())
+		}, 1000);
 
 		return () => {
-			console.log("🕰💥 Clock is being unmounted 😨")
+			console.log("🕰💥 Clock is being unmounted 😨 Stopping timer 😅")
+			clearInterval(intervalId)
 		}
 	}, [])
 
