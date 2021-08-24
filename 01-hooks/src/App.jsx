8cb1cf547d225e5cd4c51bbca4789ba9be19bddb
Johan Nordström <johan@digitalvillage.se>
2021-08-24 09:54:04 +0200

    Boilerplate.

---
+++ 01-hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+	return (
+		<div className="App">
+			<h1><span role="img" aria-label="A fish on a hook">🎣</span> React Hooks</h1>
+		</div>
+	)
+}
+
+export default App
