a398012ed333965cec79d7c39244aee9722d73c3
Johan Nordström <johan@digitalvillage.se>
2021-08-24 13:42:57 +0200

    useEffect on initial mount and when points-state is updated.

--- 01-hooks/src/components/PointsCounter.jsx
+++ 01-hooks/src/components/PointsCounter.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 
 const PointsCounter = () => {
 	const [points, setPoints] = useState(0)
+	console.log("Points:", points)
 
 	const modifyPoints = (amount) => {
 		setPoints(prevPoints => prevPoints + amount)
 	}
+
+	console.log("Rendering...")
+
+	// This will only be executed when the component is mounted,
+	// and only AFTER the component has been rendered
+	// Class Lifecycle: componentDidMount
+	useEffect(() => {
+		console.log("I'm a newly mounted component 👶🏻")
+	}, [])
+
+	// This will only be executed if `points` have changed,
+	// and only AFTER the component has been rendered
+	// Class Lifecycle: componentDidUpdate
+	useEffect(() => {
+		console.log(`Points have changed to ${points}`)
+	}, [points])
 
 	return (
 		<div className="d-flex">
