1b3ed4732a3f99cdc0e64c4a44aeaad43607f3b3
Johan Nordström <johan@digitalvillage.se>
2021-08-24 10:47:45 +0200

    Simple useState demo.

--- 01-hooks/src/App.jsx
+++ 01-hooks/src/App.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
 
 function App() {
+	const [count, setCount] = useState(42)
+
+	const decreaseCount = () => {
+		setCount(prevCount => prevCount - 1)
+		setCount(prevCount => prevCount - 1)
+	}
+
+	const increaseCount = () => {
+		setCount(prevCount => prevCount + 1)
+		setCount(prevCount => prevCount + 1)
+	}
+
+	console.log("App component re-rendered")
+
 	return (
 		<div className="App">
 			<h1><span role="img" aria-label="A fish on a hook">🎣</span> React Hooks</h1>
+
+			<div>
+				<button onClick={decreaseCount}>-</button>
+				<span>{count}</span>
+				<button onClick={increaseCount}>+</button>
+			</div>
 		</div>
 	)
 }
