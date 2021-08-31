5e80224648f62808c1d1407400c73ea885ab1ea6
Johan Nordström <johan@digitalvillage.se>
2021-08-31 13:14:25 +0200

    Added QueryClientProvider and a QueryClient to provide to our app.

--- 02-react-query/src/main.jsx
+++ 02-react-query/src/main.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 import ThemeContextProvider from './contexts/ThemeContextProvider'
 import './App.scss'
 
+const queryClient = new QueryClient()
+
 ReactDOM.render(
 	<React.StrictMode>
 
-		<BrowserRouter>
-			<ThemeContextProvider>
-				<App />
-			</ThemeContextProvider>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<ThemeContextProvider>
+					<App />
+				</ThemeContextProvider>
+			</BrowserRouter>
+		</QueryClientProvider>
 
 	</React.StrictMode>,
 	document.getElementById('root')
