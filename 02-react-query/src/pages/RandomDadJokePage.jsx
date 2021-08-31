f1ea5b00853e875d2c387f886ba9e123edbe794e
Johan Nordström <johan@digitalvillage.se>
2021-08-31 13:37:41 +0200

    Added ICanHazDadJokeAPI service, and useQuery to RandomDadJokePage

--- 02-react-query/src/pages/RandomDadJokePage.jsx
+++ 02-react-query/src/pages/RandomDadJokePage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import { useQuery } from 'react-query'
+import { getRandomJoke } from '../services/ICanHazDadJokeAPI'
 
 const RandomDadJokePage = () => {
+	const { data, error, isError, isLoading } = useQuery('random-dad-joke', getRandomJoke)
+
 	return (
 		<Container className="py-3">
 			<h1>Random Dad Joke</h1>
+
+			{isLoading && (<p>Loading dad joke dad</p>)}
+
+			{isError && (<p>An error occurred: {error}</p>)}
+
+			{data && (<div className="display-4 text-center">{data.joke}</div>)}
 		</Container>
 	)
 }
