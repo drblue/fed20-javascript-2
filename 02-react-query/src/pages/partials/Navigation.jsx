5f3da92be91e0045a73bf63e5ab0fe2971e4dc7d
Johan Nordström <johan@digitalvillage.se>
2021-08-31 11:42:23 +0200

    Added React Router to Navigation component.

--- 02-react-query/src/pages/partials/Navigation.jsx
+++ 02-react-query/src/pages/partials/Navigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
@@ -7,14 +8,22 @@ const Navigation = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
-				<div className="navbar-brand">
+				<Link to="/" className="navbar-brand">
 					<span role="img" aria-label="A person holding up a hand">🙋🏽</span> React Query
-				</div>
+				</Link>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
-							<a href="#" className="nav-link">Link</a>
+
+							<NavLink to="/link" className="nav-link">
+								Link
+							</NavLink>
+
+							<NavLink to="/another-link" className="nav-link">
+								Another Link
+							</NavLink>
+
 						</Nav>
 					</Navbar.Collapse>
 			</Container>
