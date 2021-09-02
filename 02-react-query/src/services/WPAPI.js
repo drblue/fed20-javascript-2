19c1586bd7659cf34dd9a6bd8203bc3f41298f5e
Johan Nordström <johan@digitalvillage.se>
2021-09-02 14:04:14 +0200

    Using axios instead of fetch. Also transforming response into something nicer.

--- 02-react-query/src/services/WPAPI.js
+++ 02-react-query/src/services/WPAPI.js
@@ -5,9 +5,34 @@
  * Pagination: <https://developer.wordpress.org/rest-api/using-the-rest-api/pagination/>
  */
 
-const baseURL = 'https://fed20.thehiveresistance.com/wp-json'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://fed20.thehiveresistance.com/wp-json'
 
 const get = async (endpoint) => {
+	const response = await axios.get(endpoint)
+
+	// parse link header for pagination
+	const prev = null
+	const next = "https://fed20.thehiveresistance.com/wp-json/wp/v2/posts?page=2"
+
+	// get total num of items
+	const count = parseInt(response.headers['x-wp-total'])
+
+	// get total num of pages
+	const pages = parseInt(response.headers['x-wp-totalpages'])
+
+	// transform data into something nicer
+	return {
+		count,
+		prev,
+		next,
+		pages,
+		results: response.data
+	}
+}
+
+const getFetch = async (endpoint) => {
 	const response = await fetch(baseURL + endpoint)
 
 	if (!response.ok) {
