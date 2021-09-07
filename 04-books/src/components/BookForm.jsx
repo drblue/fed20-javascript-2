46b1c9f7a45a1972737bf4bf0521dcca462db8bf
Johan Nordström <johan@digitalvillage.se>
2021-09-07 14:06:33 +0200

    Removed dummy defaultValues. Also pass a onFormSubmit-handler from parent component to `BookForm`.

--- 04-books/src/components/BookForm.jsx
+++ 04-books/src/components/BookForm.jsx
@@ -3,35 +3,36 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import { useForm } from 'react-hook-form'
 
-const BookForm = () => {
+const BookForm = ({
+	onFormSubmit,
+	defaultValues
+}) => {
 
-	const { handleSubmit, register, formState: { errors } } = useForm()
-
-	const onFormSubmit = (data) => {
-		console.log("Got me some data!", data)
-	}
+	const { handleSubmit, register, formState: { errors } } = useForm({
+		defaultValues,
+	})
 
 	return (
 		<Form onSubmit={handleSubmit(onFormSubmit)}>
 			<Form.Group className="mb-3" controlId="title">
 				<Form.Label>Title</Form.Label>
-				<Form.Control type="text" defaultValue="Banana?" {...register('title', { required: true })} placeholder="Enter book title" />
+				<Form.Control type="text" {...register('title', { required: true })} placeholder="Enter book title" />
 				{errors.title && <p>This field is required.</p>}
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="author">
 				<Form.Label>Author</Form.Label>
-				<Form.Control type="text" defaultValue="" {...register('author')}  placeholder="Enter the author's name" />
+				<Form.Control type="text" {...register('author')} placeholder="Enter the author's name" />
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="published">
 				<Form.Label>Published</Form.Label>
-				<Form.Control type="text" defaultValue="1970" {...register('published')}  placeholder="Enter year the book was published" />
+				<Form.Control type="text" {...register('published')} placeholder="Enter year the book was published" />
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="pages">
 				<Form.Label>Pages</Form.Label>
-				<Form.Control type="number" defaultValue="42" {...register('pages')}  min="0" placeholder="Enter the number of pages in the book" />
+				<Form.Control type="number" {...register('pages')} min="0" placeholder="Enter the number of pages in the book" />
 			</Form.Group>
 
 			<Button variant="success" type="submit">Submit</Button>
--- 04-books/src/pages/CreateBookPage.jsx
+++ 04-books/src/pages/CreateBookPage.jsx
@@ -7,11 +7,15 @@ import BookForm from '../components/BookForm'
 const CreateBookPage = () => {
 	const history = useHistory()
 
+	const handleFormSubmit = (data) => {
+		console.log("Got me data in parent component!", data)
+	}
+
 	return (
 		<Container className="py-3">
 			<h1>Create a new book</h1>
 
-			<BookForm />
+			<BookForm onFormSubmit={handleFormSubmit} />
 
 			<div className="mt-5">
 				<Button variant="secondary" onClick={() => history.goBack()}>&laquo; Go back</Button>
