46fa12d1112fea15809b0dedb64d07a132040430
Johan Nordström <johan@digitalvillage.se>
2021-09-07 13:48:51 +0200

    Using react-hooks-form instead of manually managing form-state.

--- 04-books/src/components/BookForm.jsx
+++ 04-books/src/components/BookForm.jsx
@@ -1,50 +1,37 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import { useForm } from 'react-hook-form'
 
 const BookForm = () => {
-	const [data, setData] = useState({
-		title: "",
-		author: "",
-		published: 2021,
-		pages: 1
-	})
 
-	const handleInputChange = (e) => {
-		setData(prevData => {
-			return {
-				...prevData,
-				[e.target.id]: e.target.value
-			}
-		})
-	}
+	const { handleSubmit, register, formState: { errors } } = useForm()
 
-	const handleFormSubmit = (e) => {
-		e.preventDefault()
-
-		console.log("Data to send:", data)
+	const onFormSubmit = (data) => {
+		console.log("Got me some data!", data)
 	}
 
 	return (
-		<Form onSubmit={handleFormSubmit}>
+		<Form onSubmit={handleSubmit(onFormSubmit)}>
 			<Form.Group className="mb-3" controlId="title">
 				<Form.Label>Title</Form.Label>
-				<Form.Control type="text" onChange={handleInputChange} value={data.title} placeholder="Enter book title" />
+				<Form.Control type="text" defaultValue="Banana?" {...register('title', { required: true })} placeholder="Enter book title" />
+				{errors.title && <p>This field is required.</p>}
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="author">
 				<Form.Label>Author</Form.Label>
-				<Form.Control type="text" onChange={handleInputChange} value={data.author} placeholder="Enter the author's name" />
+				<Form.Control type="text" defaultValue="" {...register('author')}  placeholder="Enter the author's name" />
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="published">
 				<Form.Label>Published</Form.Label>
-				<Form.Control type="text" onChange={handleInputChange} value={data.published} placeholder="Enter year the book was published" />
+				<Form.Control type="text" defaultValue="1970" {...register('published')}  placeholder="Enter year the book was published" />
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="pages">
 				<Form.Label>Pages</Form.Label>
-				<Form.Control type="number" onChange={handleInputChange} value={data.pages} min="0" placeholder="Enter the number of pages in the book" />
+				<Form.Control type="number" defaultValue="42" {...register('pages')}  min="0" placeholder="Enter the number of pages in the book" />
 			</Form.Group>
 
 			<Button variant="success" type="submit">Submit</Button>
