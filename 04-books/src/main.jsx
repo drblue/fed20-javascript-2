221a998ecfcfa4bd33910c84c5eb5953adf8e309
Johan Nordström <johan@digitalvillage.se>
2021-09-09 14:33:35 +0200

    More reasonable stale and cache times.

--- 04-books/src/main.jsx
+++ 04-books/src/main.jsx
@@ -11,7 +11,8 @@ const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
 			refetchOnWindowFocus: false,
-			staleTime: 1000 * 10, // 10 seconds
+			staleTime: 1000 * 60 * 2, // 2 minutes
+			cacheTime: 1000 * 60 * 60 * 4, // 4 hours
 		},
 	},
 })
