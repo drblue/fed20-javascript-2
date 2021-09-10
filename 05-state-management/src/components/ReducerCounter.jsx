01dca9fb3c62435569062bdfdb6d464fc17684b2
Johan Nordström <johan@digitalvillage.se>
2021-09-10 10:44:12 +0200

    Added useReducer along with a reducer and the initialState.

--- 05-state-management/src/components/ReducerCounter.jsx
+++ 05-state-management/src/components/ReducerCounter.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 
+const initialState = {
+	count: 0,
+}
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'increment':
+			return {
+				count: state.count + 1
+			}
+		case 'decrement':
+			return {
+				count: state.count - 1
+			}
+		default:
+			throw new Error('Unkown action type: ' + action.type)
+	}
+}
+
 const ReducerCounter = () => {
+	const [state, dispatch] = useReducer(reducer, initialState)
 
 	return (
-		<div className="reducer-counter">
-			<Button variant="warning" onClick={() => null}>-</Button>
+		<div className="d-flex">
+			<Button variant="warning" onClick={() => dispatch({ type: 'decrement' })}>-</Button>
 
-			<span className="points">{null}</span>
+			<span className="points">{state.count}</span>
 
-			<Button variant="success" onClick={() => null}>+</Button>
+			<Button variant="success" onClick={() => dispatch({ type: 'increment' })}>+</Button>
 		</div>
 	)
 }
 
-export default ReducerCounter
+export default ReducerCounter;
