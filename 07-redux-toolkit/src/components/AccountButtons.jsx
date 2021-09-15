5c39a01d0215f440f8bf6e0d36731bc94de9d179
Johan Nordström <johan@digitalvillage.se>
2021-09-16 01:01:36 +0200

    Redux Toolkit skeleton.

---
+++ 07-redux-toolkit/src/components/AccountButtons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+const AccountButtons = () => {
+
+	return (
+		<div>
+			<Button variant="success" onClick={() => {}}>Deposit</Button>
+			<Button variant="warning" onClick={() => {}}>Withdraw</Button>
+		</div>
+	)
+}
+
+export default AccountButtons
