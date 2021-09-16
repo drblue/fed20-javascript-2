2a6fce68d43e4b399446de6e45e96df44b62f537
Johan Nordström <johan@digitalvillage.se>
2021-09-16 09:54:30 +0200

    Created Store and provided it to our app.

--- 07-redux-toolkit/src/main.jsx
+++ 07-redux-toolkit/src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './App.scss'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
 
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 
 		</BrowserRouter>
 	</React.StrictMode>,
