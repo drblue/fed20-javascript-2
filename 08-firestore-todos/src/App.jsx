5d40000e4cf09d803319f9d6e920cc6d5f7e6dd7
Johan Nordström <johan@digitalvillage.se>
2021-12-09 13:01:49 +0100

    - Added routes to LoginPage, LogoutPage and SignupPage. - Wrapped app in a Container instead of a div. - Also added some nice padding.

--- 08-firestore-todos/src/App.jsx
+++ 08-firestore-todos/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { Container } from 'react-bootstrap'
 import HomePage from './pages/HomePage'
 import Navigation from './pages/partials/Navigation'
 import PageNotFound from './pages/PageNotFound'
@@ -12,14 +13,18 @@ function App() {
 		<>
 			<Navigation />
 
-			<div id="App">
+			<Container id="App" className="py-3">
 				<Routes>
 					<Route path="/" element={<HomePage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/logout" element={<LogoutPage />} />
+					<Route path="/signup" element={<SignupPage />} />
 					<Route path="/todos" element={<TodosPage />} />
 					<Route path="/todos/:id" element={<TodoPage />} />
 					<Route element={<PageNotFound />} />
 				</Routes>
-			</div>
+			</Container>
+
 			<ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
 		</>
 	)
