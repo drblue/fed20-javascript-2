2eabb08758930ae382aa21d510fe5593da01d9b6
Johan Nordström <johan@digitalvillage.se>
2021-12-09 12:20:05 +0100

    Skeleton pages for Firebase Auth demo.

---
+++ 08-firestore-todos/src/pages/SignupPage.jsx
@@ -0,0 +1,69 @@
+import React, { useRef, useState } from 'react'
+import { Row, Col, Form, Button, Card, Alert } from 'react-bootstrap'
+import { Link, useNavigate } from 'react-router-dom'
+
+const SignupPage = () => {
+	const emailRef = useRef()
+	const passwordRef = useRef()
+	const passwordConfirmRef = useRef()
+	const [error, setError] = useState(null)
+	const [loading, setLoading] = useState(false)
+	const navigate = useNavigate()
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+
+		// make sure user has entered the same password in both input fields
+		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+			return setError("The passwords does not match")
+		}
+
+		setError(null);
+	}
+
+	return (
+		<>
+			<Row>
+				<Col md={{ span: 6, offset: 3 }}>
+					<Card>
+						<Card.Body>
+							<Card.Title className="mb-3">Sign Up</Card.Title>
+
+							{error && (<Alert variant="danger">{error}</Alert>)}
+
+							<Form onSubmit={handleSubmit}>
+
+								<Form.Group id="email" className="mb-3">
+									<Form.Label>Email</Form.Label>
+									<Form.Control type="email" ref={emailRef} required />
+								</Form.Group>
+
+								<Form.Group id="password" className="mb-3">
+									<Form.Label>Password</Form.Label>
+									<Form.Control type="password" ref={passwordRef} required />
+								</Form.Group>
+
+								<Form.Group id="password-confirm" className="mb-3">
+									<Form.Label>Password Confirmation</Form.Label>
+									<Form.Control type="password" ref={passwordConfirmRef} required />
+								</Form.Group>
+
+								<Button disabled={loading} type="submit">Create Account</Button>
+							</Form>
+
+							<div className="text-center mt-3">
+								<Link to="/forgot-password">Forgot Password?</Link>
+							</div>
+						</Card.Body>
+					</Card>
+
+					<div className="text-center mt-3">
+						Already have an account? <Link to="/login">Log In</Link>
+					</div>
+				</Col>
+			</Row>
+		</>
+	)
+}
+
+export default SignupPage
