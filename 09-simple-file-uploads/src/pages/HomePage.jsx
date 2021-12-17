f162bcf22d8ed1c5795ce3bdeb032ae55b68dc0d
Johan Nordström <johan@digitalvillage.se>
2021-12-17 10:07:51 +0100

    Show upload progress.

--- 09-simple-file-uploads/src/pages/HomePage.jsx
+++ 09-simple-file-uploads/src/pages/HomePage.jsx
@@ -3,13 +3,14 @@ import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
-import { ref, getDownloadURL, uploadBytes } from 'firebase/storage'
+import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
 import { storage } from '../firebase'
 
 const HomePage = () => {
 	const [image, setImage] = useState(null)
 	const [message, setMessage] = useState()
 	const [uploadedImageUrl, setUploadedImageUrl] = useState()
+	const [uploadProgress, setUploadProgress] = useState(null)
 
 	const handleFileChange = (e) => {
 		if (e.target.files[0]) {
@@ -21,6 +22,8 @@ const HomePage = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 
+		setUploadProgress(null)
+
 		if (!image) {
 			return
 		}
@@ -28,36 +31,38 @@ const HomePage = () => {
 		// create a reference to upload the file to
 		const fileRef = ref(storage, image.name)
 
-		try {
-			// upload image to fileRef
-			const uploadResult = await uploadBytes(fileRef, image)
+		// upload image to fileRef
+		const uploadTask = uploadBytesResumable(fileRef, image)
 
-			setMessage({
-				type: "success",
-				msg: "Image successfully uploaded 🤩",
-			})
+		// attach upload observer
+		uploadTask.on('state_changed', (uploadTaskSnapshot) => {
+			setUploadProgress(Math.round((uploadTaskSnapshot.bytesTransferred / uploadTaskSnapshot.totalBytes) * 100))
 
-			// reference to the uploaded file
-			const uploadedFileRef = uploadResult.ref
-
-			// get download url to uploaded file
-			const url = await getDownloadURL(uploadedFileRef)
-
-			// set uploadedImageUrl to the url
-			setUploadedImageUrl(url)
-
-		} catch (e) {
+		}, (error) => {
 			console.log("NOT so great success, fail!", e)
 
 			setMessage({
 				type: "warning",
 				msg: `Image failed to upload due to the following error: ${e.message}`,
 			})
-		}
+		}, async () => {
+			setMessage({
+				type: "success",
+				msg: "Image successfully uploaded 🤩",
+			})
+
+			// get download url to uploaded file
+			const url = await getDownloadURL(fileRef)
+
+			// set uploadedImageUrl to the url
+			setUploadedImageUrl(url)
+		})
 	}
 
 	const handleReset = () => {
 		setImage(null)
+		setMessage(null)
+		setUploadProgress(null)
 	}
 
 	return (
@@ -83,6 +88,8 @@ const HomePage = () => {
 				<Button type="submit" variant="primary">Upload</Button>
 				<Button type="reset" variant="danger">Reset</Button>
 			</Form>
+
+			{uploadProgress && (<p>Uploading... {uploadProgress} %</p>)}
 
 			{uploadedImageUrl && <img src={uploadedImageUrl} className="img-fluid" />}
 		</Container>
