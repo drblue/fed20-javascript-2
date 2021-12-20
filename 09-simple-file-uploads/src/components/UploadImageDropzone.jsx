1d02097605a28adfdb88faf7390393f6d35ca2da
Johan Nordström <johan@digitalvillage.se>
2021-12-20 11:51:53 +0100

    Accept only images.

--- 09-simple-file-uploads/src/components/UploadImageDropzone.jsx
+++ 09-simple-file-uploads/src/components/UploadImageDropzone.jsx
@@ -6,15 +6,21 @@ const UploadImageDropzone = () => {
 		console.log("Got me zum files 😊", acceptedFiles)
 	}, [])
 
-	const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+	const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
+		accept: 'image/gif, image/jpeg, image/png, image/webp',
+		onDrop,
+	})
 
 	return (
-		<div {...getRootProps()} id="upload-image-dropzone-wrapper">
+		<div {...getRootProps()}
+			id="upload-image-dropzone-wrapper"
+			className={`${isDragAccept ? 'drag-accept' : ''} ${isDragReject ? 'drag-reject' : ''}`}
+		>
 			<input {...getInputProps()} />
 
 			{
 				isDragActive
-					? <p>Drop it like its hot 🔥!</p>
+					? (isDragAccept ? <p>Drop it like its hot 🔥!</p> : <p>We don't want that file 😡!</p>)
 					: <p>Give me some files 😋!</p>
 			}
 		</div>
