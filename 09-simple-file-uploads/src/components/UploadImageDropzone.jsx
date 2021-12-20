1095fd1416d0124eeb411538ccc9463ab9d68c45
Johan Nordström <johan@digitalvillage.se>
2021-12-20 11:59:43 +0100

    Style dropzone if file being dropped is valid or not.

--- 09-simple-file-uploads/src/components/UploadImageDropzone.jsx
+++ 09-simple-file-uploads/src/components/UploadImageDropzone.jsx
@@ -6,7 +6,7 @@ const UploadImageDropzone = () => {
 		console.log("Got me zum files 😊", acceptedFiles)
 	}, [])
 
-	const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
+	const { getRootProps, getInputProps, acceptedFiles, isDragActive, isDragAccept, isDragReject } = useDropzone({
 		accept: 'image/gif, image/jpeg, image/png, image/webp',
 		onDrop,
 	})
@@ -23,6 +23,16 @@ const UploadImageDropzone = () => {
 					? (isDragAccept ? <p>Drop it like its hot 🔥!</p> : <p>We don't want that file 😡!</p>)
 					: <p>Give me some files 😋!</p>
 			}
+
+			{acceptedFiles && (
+				<div className="accepted-files mt-2">
+					<ul className="list-unstyled">
+						{acceptedFiles.map(file => (
+							<li key={file.name}>{file.name} ({Math.round(file.size / 1024)} kb)</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	)
 }
