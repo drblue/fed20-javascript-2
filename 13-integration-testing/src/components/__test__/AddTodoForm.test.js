875b7e7d947d5c236a9a8b82911d542b1e8196ba
Johan Nordström <johan@digitalvillage.se>
2022-01-04 11:09:14 +0100

    Test that we can type into the input field for new todos.

--- 13-integration-testing/src/components/__test__/AddTodoForm.test.js
+++ 13-integration-testing/src/components/__test__/AddTodoForm.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import AddTodoForm from '../AddTodoForm'
 
 it("renders input field initally empty", () => {
@@ -7,4 +8,16 @@ it("renders input field initally empty", () => {
 	const inputEl = screen.getByRole("textbox")
 
 	expect(inputEl.value).toBe("")
+})
+
+test("can type into input field", () => {
+	render(<AddTodoForm />)
+
+	const inputEl = screen.getByRole("textbox")
+
+	// type into input field
+	userEvent.type(inputEl, "A new todo")
+
+	// expect input field value to be what we just typed into it
+	expect(inputEl.value).toBe("A new todo")
 })
