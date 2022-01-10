b52a7ea938e0cce7e8dfedabad1ad6b950b88073
Johan Nordström <johan@digitalvillage.se>
2022-01-11 00:49:50 +0100

    (mostly) Empty Vite React TypeScript skeleton.

---
+++ 14-typescript-todos/src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap'
+
+const App = () => {
+	return (
+		<Container id="App" className="py-3">
+			<h1>Todos</h1>
+
+		</Container>
+	)
+}
+
+export default App
